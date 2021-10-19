abe3b307d57e6dc73d20690370f611652a85e575
javierpb275 <javierpb275@gmail.com>
2021-10-19 17:43:57 +0200

    created fullName using methods object for the vm and displayed fullName() on the page

--- section-2/app.js
+++ section-2/app.js
@@ -5,8 +5,9 @@ const vm = Vue.createApp({
       lastName: "Doe",
     };
   },
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+    },
+  },
 }).mount("#app");
-
-setTimeout(() => {
-  vm.firstName = "Bob";
-}, 2000);
