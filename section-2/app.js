f24e0025d13172adfb7eff99dcaea94fa03a4367
javierpb275 <javierpb275@gmail.com>
2021-10-21 17:33:07 +0200

    using keyboard modifiers: @keyup.enter and @click.ctrl

--- section-2/app.js
+++ section-2/app.js
@@ -2,6 +2,7 @@ const vm = Vue.createApp({
   data() {
     return {
       firstName: "John",
+      middleName: "",
       lastName: "Doe",
       url: "https://google.com",
       raw_url: '<a href="https://google.com" target="_blank">Google</a>',
@@ -10,7 +11,9 @@ const vm = Vue.createApp({
   },
   methods: {
     fullName() {
-      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+      return `${this.firstName} ${
+        this.middleName
+      } ${this.lastName.toUpperCase()}`;
     },
     increment() {
       this.age++;
@@ -18,6 +21,9 @@ const vm = Vue.createApp({
     updateLastName(msg, event) {
       console.log(msg);
       this.lastName = event.target.value;
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value;
     },
   },
 }).mount("#app");
