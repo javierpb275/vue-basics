9dc36d0bf374e66d7777504b1ea3a75dc5f24e33
javierpb275 <javierpb275@gmail.com>
2021-10-27 18:12:21 +0200

    components

--- section-4/section-4-1/app.js
+++ section-4/section-4-1/app.js
@@ -1,21 +1,12 @@
-let vm = Vue.createApp({
+let vm = Vue.createApp({});
+
+vm.component("hello", {
+  template: `<h1>{{message}}</h1>`,
   data() {
     return {
-      message: "Hello world!",
+      message: "Hello World!",
     };
   },
-  template: `{{message}}`,
 });
 
 vm.mount("#app");
-
-let vm2 = Vue.createApp({
-  data() {
-    return {
-      message: "Hello world!",
-    };
-  },
-  render() {
-    return Vue.h("h1", this.message);
-  },
-}).mount("#app2");
