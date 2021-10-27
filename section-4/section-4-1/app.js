e043403122db57e22b282f6a62e02a42767aa08e
javierpb275 <javierpb275@gmail.com>
2021-10-27 17:41:02 +0200

    different ways of rendering the template

--- section-4/section-4-1/app.js
+++ section-4/section-4-1/app.js
@@ -4,39 +4,18 @@ let vm = Vue.createApp({
       message: "Hello world!",
     };
   },
-  methods: {
-    changeMessage(whatever) {
-      this.message = whatever;
-    },
-  },
-  beforeCreate() {
-    console.log("beforeCreate() function called!", this.message);
-  },
-  created() {
-    console.log("created() function called!", this.message);
-  },
-  beforeMount() {
-    console.log("beforeMount() function called!", this.$el);
-  },
-  mounted() {
-    console.log("mounted() function called!", this.$el);
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate() function called!");
-  },
-  updated() {
-    console.log("updated() function called!");
-  },
-  beforeUnmount() {
-    console.log("beforeUnmount() function called!");
-  },
-  unmounted() {
-    console.log("unmounted() function called!");
-  },
+  template: `{{message}}`,
 });
 
 vm.mount("#app");
 
-/* setTimeout(() => {
-  vm.mount("#app");
-}, 3000); */
+let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+  render() {
+    return Vue.h("h1", this.message);
+  },
+}).mount("#app2");
